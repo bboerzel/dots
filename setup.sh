c47d6bbe25f362304f213af46820003f1bdec7b9
Benjamin Boerzel <ben@boerzel.de>
2020-08-03 07:02:35 +0000

    Setup changed

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 cp -r . .. 
 #rm -rf dotfiles
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+base16_dracula
 #mkdir tmp
 #cd tmp
 #wget https://github.com/Peltoche/lsd/releases/download/0.17.0/lsd_0.17.0_amd64.deb
